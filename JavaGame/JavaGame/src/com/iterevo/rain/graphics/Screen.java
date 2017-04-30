2da416beff08b050033b372908e6f33d9d2b9470
Matt Behnke <iterevo@gmail.com>
2017-04-30 00:11:29 -0500

    the update

--- JavaGame/JavaGame/src/com/iterevo/rain/graphics/Screen.java
+++ JavaGame/JavaGame/src/com/iterevo/rain/graphics/Screen.java
@@ -2,6 +2,8 @@ package com.iterevo.rain.graphics;
 
 import java.util.Random;
 
+import com.iterevo.rain.level.tile.Tile;
+
 public class Screen {
 
 	private int width, height;
@@ -41,4 +43,14 @@ public class Screen {
 		}
 	}
 	
+	public void renderTile(int xp, int yp, Tile tile) {
+		for(int y = 0; y < tile.sprite.SIZE; y++) {
+			int ya = y + yp;
+			for(int x = 0; x < tile.sprite.SIZE; x++) {
+				int xa = y + xp;
+				if(xa < 0 || xa >= width || ya < 0 || ya >= width) break;
+				pixels[xa*ya*width] = tile.sprite.pixels[x+y*tile.sprite.SIZE];
+			}
+		}
+	}
 }
